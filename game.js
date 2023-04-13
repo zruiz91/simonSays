f8e4a4b965ece51e57d3d6835574127854d6009b
Zach Ruiz <powersbooth10@gmail.com>
2023-04-13 13:10:21 -0500

    added sound to play when the buttons are clicked.

--- game.js
+++ game.js
@@ -2,24 +2,38 @@ let gamePattern = [];
 
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+let blueAudio = new Audio("sounds/blue.mp3");
+
+let greenAudio = new Audio("sounds/green.mp3");
+
+let redAudio = new Audio("sounds/red.mp3");
+
+let yellowAudio = new Audio("sounds/yellow.mp3");
+
+let wrongAudio = new Audio("sounds/wrong.mp3");
+
 //adds event listener for the button to flash when clicked
 $("#yellow").click(function () {
     $("#yellow").fadeOut(100).fadeIn(100)
+    yellowAudio.play();
 });
 
 //adds event listener for the button to flash when clicked
 $("#blue").click(function () {
     $("#blue").fadeOut(100).fadeIn(100)
+    blueAudio.play();
 });
 
 //adds event listener for the button to flash when clicked
 $("#red").click(function () {
     $("#red").fadeOut(100).fadeIn(100)
+    redAudio.play();
 });
 
 //adds event listener for the button to flash when clicked
 $("#green").click(function () {
     $("#green").fadeOut(100).fadeIn(100)
+    greenAudio.play();
 });
 
 
