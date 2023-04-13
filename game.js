22fbf1abb0565df246d0969feccd5c9cde25faa0
Zach Ruiz <powersbooth10@gmail.com>
2023-04-12 22:41:49 -0500

    added event listeners to buttons that initiates a flash animation when clicked.

--- game.js
+++ game.js
@@ -2,8 +2,25 @@ let gamePattern = [];
 
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+//adds event listener for the button to flash when clicked
+$("#yellow").click(function () {
+    $("#yellow").fadeOut(100).fadeIn(100)
+});
 
+//adds event listener for the button to flash when clicked
+$("#blue").click(function () {
+    $("#blue").fadeOut(100).fadeIn(100)
+});
 
+//adds event listener for the button to flash when clicked
+$("#red").click(function () {
+    $("#red").fadeOut(100).fadeIn(100)
+});
+
+//adds event listener for the button to flash when clicked
+$("#green").click(function () {
+    $("#green").fadeOut(100).fadeIn(100)
+});
 
 
 //function that generates random number (0,1,2,3)
